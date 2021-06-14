23ecb047ad216919c275e8e689abfb3899f810ed
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-06-14 17:54:07 -0500

    feat: Made initial mockup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="logo">
+        M
+      </div>
+      <h1 className="text">MINGO</h1>
+      <h4 className="text">New to the area?</h4>
+      <button>GET STARTED</button>
+      <h5 className="accent">Founder: Harry Bethancourt</h5>
+      <span className="accent">Highrise Elite Marketing</span>
     </div>
   );
 }
