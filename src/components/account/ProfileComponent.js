7b2af0638bf4d467a41bd78fd1a17a7bd1a913bf
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-07-09 22:25:23 -0500

    feat: initial for submit for search params

--- src/components/account/ProfileComponent.js
+++ src/components/account/ProfileComponent.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function ProfileComponent() {
-    return (
-        <div>
-            <h1>Profile Page</h1>
-        </div>
-    )
+export default class ProfileComponent extends Component {
+    constructor (props){
+        super(props)
+        this.state = {
+
+        }
+
+    }
+
+    submitAnswers = () => {
+
+    }
+    render() {
+        return (
+            <div>
+                <h2>Profile</h2>
+                <form>
+                    <input type="number" value={40}/>
+                    <select name="unit">
+                        <option value="miles">Miles</option>
+                        <option value="feet">Feet</option>
+                    </select>
+                </form>
+            </div>
+        )
+    }
 }
+
+
+
+// import React from 'react'
+
+// export default function ProfileComponent() {
+//     return (
+//         <div>
+//             <h1>Profile Page</h1>
+            
+//         </div>
+//     )
+// }
